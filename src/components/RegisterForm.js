47a05735807fd72dbb8cefe244fcde6e7878fcd6
janlat <jaaneq@gmail.com>
2016-08-09 12:50:02 +0200

    add register form steps

--- src/components/RegisterForm.js
+++ src/components/RegisterForm.js
@@ -1,29 +1,62 @@
 import React from 'react';
+import cx from 'classnames';
 
 import Counter from './Counter';
 
 export default React.createClass({
 
+	getInitialState: function() {
+		this.inputs = {};
+		return { step: 'start' };
+	},
+
+	next: function(ev) {
+		ev.preventDefault();
+		console.log(this.inputs);
+		this.setState({ step: 'details' });
+	},
+
 	render: function() {
+		let setRef = name => ref => this.inputs[name] = ref;
+
 		return (
 			<div id="register">
 				<div className="wrap" id="register-form">
-					<h2>Nagłówek zachęcający do zapisania</h2>
-					<form>
-						<div>
-							<label htmlFor="name">Nazwa restauracji</label>
-							<input type="text" id="name" />
-						</div>
-						<div>
-							<label htmlFor="addres">Adres restauracji</label>
-							<input type="text" id="address" />
-						</div>
-						<div>
-							<label htmlFor="email">Adres e-mail</label>
-							<input type="text" id="email" />
-						</div>
-						<button className="form-button" type="submit">Zapisz mnie</button>
-					</form>
+
+					<div className={cx('step', {active: this.state.step == 'start'})}>
+						<h2>Nagłówek zachęcający do zapisania</h2>
+						<form onSubmit={this.next}>
+							<div className="form-row">
+								<label htmlFor="name">Nazwa restauracji</label>
+								<input type="text" id="name" ref={setRef('name')} />
+							</div>
+							<div className="form-row">
+								<label htmlFor="address">Adres restauracji</label>
+								<input type="text" id="address" ref={setRef('address')} />
+							</div>
+							<div className="form-row">
+								<label htmlFor="city">Miasto</label>
+								<input type="text" id="city" ref={setRef('city')} />
+							</div>
+							<button className="form-button" type="submit">Dalej</button>
+						</form>
+					</div>
+
+					<div className={cx('step', 'slide', {active: this.state.step == 'details'})}>
+						<h2>Jeszcze parę szczegółów</h2>
+						<form>
+							<div className="form-row">
+								<label htmlFor="email">Adres e-mail</label>
+								<input type="text" id="email" ref={setRef('email')} />
+							</div>
+							<div className="form-row">
+								<label htmlFor="passwd">Hasło</label>
+								<input type="text" id="passwd" ref={setRef('passwd')} />
+							</div>
+							<button className="form-button" onClick={ (ev) => {this.setState({step:'start'});ev.preventDefault() }}>Zapisz mnie</button>
+						</form>
+					</div>
+
 				</div>
 			</div>
 		);
--- src/index.js
+++ src/index.js
@@ -10,14 +10,24 @@ import AuthorizedOnly from './components/pages/AuthorizedOnly';
 
 import './styles/main.css';
 
+import { Provider } from 'react-redux';
+import ReduxTest, { store } from './components/ReduxTest';
+
+store.dispatch({type: 'ADD_TASK', title: 'pierwsze'})
+store.dispatch({type: 'ADD_TASK', title: 'drugie'})
+store.dispatch({type: 'ADD_TASK', title: 'trzecie'})
+
 render(
-	<Router history={browserHistory}>
-		<Route path="/" component={App}>
-			<IndexRoute component={RegisterForm} />
-			<Route path="info" component={AuthorizedOnly(Dummy)} />
-			<Route path="profile" component={Dummy} />
-			<Router path="*" component={ErrorPage} />
-		</Route>
-	</Router>,
+	<Provider store={store}>
+		<Router history={browserHistory}>
+			<Route path="/" component={App}>
+				<IndexRoute component={RegisterForm} />
+				<Route path="test" component={ReduxTest} />
+				<Route path="info" component={AuthorizedOnly(Dummy)} />
+				<Route path="profile" component={Dummy} />
+				<Router path="*" component={ErrorPage} />
+			</Route>
+		</Router>
+	</Provider>,
 	document.getElementById('app')
 );
