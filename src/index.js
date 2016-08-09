7e182d0751e83a996e9a4797595bbcbca5ca0e7f
janlat <jaaneq@gmail.com>
2016-08-09 12:58:51 +0200

    remove test components

--- src/index.js
+++ src/index.js
@@ -11,23 +11,15 @@ import AuthorizedOnly from './components/pages/AuthorizedOnly';
 import './styles/main.css';
 
 import { Provider } from 'react-redux';
-import ReduxTest, { store } from './components/ReduxTest';
-
-store.dispatch({type: 'ADD_TASK', title: 'pierwsze'})
-store.dispatch({type: 'ADD_TASK', title: 'drugie'})
-store.dispatch({type: 'ADD_TASK', title: 'trzecie'})
 
 render(
-	<Provider store={store}>
-		<Router history={browserHistory}>
-			<Route path="/" component={App}>
-				<IndexRoute component={RegisterForm} />
-				<Route path="test" component={ReduxTest} />
-				<Route path="info" component={AuthorizedOnly(Dummy)} />
-				<Route path="profile" component={Dummy} />
-				<Router path="*" component={ErrorPage} />
-			</Route>
-		</Router>
-	</Provider>,
+	<Router history={browserHistory}>
+		<Route path="/" component={App}>
+			<IndexRoute component={RegisterForm} />
+			<Route path="info" component={AuthorizedOnly(Dummy)} />
+			<Route path="profile" component={Dummy} />
+			<Router path="*" component={ErrorPage} />
+		</Route>
+	</Router>,
 	document.getElementById('app')
 );
