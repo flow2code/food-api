76151f8a57638ec2cb5bbdb9678f0adf25b2a17b
janlat <jan.latour@flow2code.com>
2016-08-19 14:39:25 +0200

    add webpack debug tool
    shows error's source

--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 let dev = {
+  devtool: 'eval',
   entry: [
     'webpack-dev-server/client',
     'webpack/hot/only-dev-server',
